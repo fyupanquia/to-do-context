9718cd6f3301cc3bf225b18f91748f227ea57260
FYUPANQUIA <fyupanquia@outlook.com>
2020-02-04 14:53:06 -0500

    localstorage

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,29 +1,19 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer, useEffect } from "react";
 import { bookReducer } from '../reducers/bookReducer';
 
 export const BookContext = createContext();
 
 const BookContextProvider = ({ children }) => {
-    /*
-    const [books, setBooks] = useReducer(bookReducer,[
-    	{'title': "cmb 1", author:'author 1', id:1},
-    	{'title': "cmb 2", author:'author 2', id:2},
-    	{'title': "cmb 3", author:'author 3', id:3},
-    ]);
-    */
-    const [books, dispatch ] = useReducer(bookReducer,[]);
-    /*
-    const state = {
-        books
-    };
-    const addBook = ({title, author}) => {
-    	setBooks([...books, {title, author, id: (books.length +1) }])
-    }
 
-    const removeBook = (id) => {
-    	setBooks(books.filter(book => book.id!== id));
-    }
-    */
+    const [books, dispatch ] = useReducer(bookReducer,[], () => {
+        const localData = localStorage.getItem('books');
+        return localData ? JSON.parse(localData) : [];
+    });
+
+    useEffect( () => {
+        localStorage.setItem('books', JSON.stringify(books));
+    }, [books] );
+
     return <BookContext.Provider value={{ books, dispatch}} >
         {children}
     </BookContext.Provider>
